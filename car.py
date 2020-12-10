c00ae1ca083ed0bdb6668de9451a1b8ae910d078
vijmanan04 <75040859+vijmanan04@users.noreply.github.com>
2020-12-09 21:29:12 -0500

    Update car.py

--- car.py
+++ car.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
 import RPi.GPIO as GPIO
+import time
 from time import sleep
+
+#Basic convenience
+GPIO.setwarnings(0)
+
 
 #set up numbering for points
 GPIO.setmode(GPIO.BCM)
@@ -12,39 +17,55 @@ in2 = 25
 enA = 23
 
 #Set up all pins as output pins
-GPIO.setup(in1, GPIO.OUT)
-GPIO.setup(in2, GPIO.OUT)
-GPIO.setup(enA, GPIO.OUT)
+def setup():
+    GPIO.setup(in1, GPIO.OUT)
+    GPIO.setup(in2, GPIO.OUT)
+    GPIO.setup(enA, GPIO.OUT)
 
 #setup PWM Cycle
 #pCycle = p.(enA, 1000)
+    
+#Stop function   #not Off function
+def stop():
+    setup()
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(enA, GPIO.LOW)
+    
 
 #Forward function
 def forward():
+    setup()
     GPIO.output(in1, GPIO.HIGH)
     GPIO.output(in2, GPIO.LOW)
     GPIO.output(enA, GPIO.HIGH)
-    GPIO.cleanup()
+    time.sleep(1)
+    stop()
 
 #Backward function
 def backward():
+    setup()
     GPIO.output(in1, GPIO.LOW)
     GPIO.output(in2, GPIO.HIGH)
     GPIO.output(enA, GPIO.HIGH)
-    GPIO.cleanup()
+    time.sleep(1)
+    stop()
 
 #User Interface + command prompt
 def UI():
     while(1):
-        ui = input()
-        ui = str(ui)
-        if (ui == "w"):
+        while (ui == "w"):
             print("FORWARD")
             forward()
-        if (ui == "b"):
+            UI()
+        while (ui == "s"):
             print("BACKWARD")
             backward()
+            UI()
+        
         else:
-            print("INVALID INPUT... SHUTTING DOWN")
+            print("INVALID INPUT... ENTER AGAIN")
             GPIO.cleanup()
-            break()
+            exit()
+
+
