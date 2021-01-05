bb78308e5ef1ae6eb843014bd91fc65ee66b2581
vijmanan04 <75040859+vijmanan04@users.noreply.github.com>
2021-01-04 22:14:41 -0500

    Update and rename client.py to intitialAttempts/WKeyboard/client.py

--- intitialAttempts/WKeyboard/client.py
+++ intitialAttempts/WKeyboard/client.py
@@ -2,10 +2,10 @@ import socket
 import drive
 
 
-host = "192.168.1.180" #ip of computer
+host = "000.000.0.000" #ip of computer
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #set instance of socket
-s.connect((host, 5000)) #connect to computer using port 5000
+s.connect((host, xxxx)) #connect to computer using port xxx
 
 msg = s.recv(1024) #recieve data in bits of 1024
 print(msg.decode("utf-8")) #print data and decode as utf-8
